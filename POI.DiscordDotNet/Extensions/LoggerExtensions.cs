d160a1dd50ea5be1bea98a07ae8cba6bf5c787ed
Eris <erisapps@outlook.be>
2021-04-19 22:31:48 +0200

    Improved Score command error logging

--- POI.DiscordDotNet/Extensions/LoggerExtensions.cs
+++ POI.DiscordDotNet/Extensions/LoggerExtensions.cs
@@ -1,15 +1,19 @@
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
+using Microsoft.Extensions.Logging;
 
 namespace POI.DiscordDotNet.Extensions
 {
-	internal static class CommandContextExtensions
+	internal static class LoggerExtensions
 	{
-		internal static async Task Errored(this CommandContext ctx, string? errorDetails = null, bool shouldDelete = true)
+		internal static async Task LogError(this ILogger logger, CommandContext ctx, string? errorDetails = null, bool shouldDelete = true)
 		{
 			var errorMessage = await ctx.RespondAsync("I'm really sorry, but something went wrong. Please don't get mad at me, but ask try asking me again at a later time :(" +
-			                        $"{(string.IsNullOrWhiteSpace(errorDetails) ? string.Empty : $"\nDetails: {errorDetails}")}");
+			                                          $"{(string.IsNullOrWhiteSpace(errorDetails) ? string.Empty : $"\nDetails: {errorDetails}")}");
+
+			logger.LogError("{ErrorMessage}", errorMessage);
+
 			if (shouldDelete)
 			{
 				await Task.Delay(TimeSpan.FromSeconds(5));
