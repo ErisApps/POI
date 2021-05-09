e7e14ed36db562ba4d443a66a192fbd5dfe0b1ff
Eris <erisapps@outlook.be>
2021-05-09 13:02:25 +0200

    Added API-key middleware

--- POI.Core/Services/ConstantsCore.cs
+++ POI.Core/Services/ConstantsCore.cs
@@ -5,6 +5,8 @@ namespace POI.Core.Services
 {
 	public abstract class ConstantsCore : IConstantsCore
 	{
+		public const string API_KEY_HEADER_NAME = "X-API-Key";
+
 		public abstract string Name { get; }
 		public abstract Version Version { get; }
 	}
