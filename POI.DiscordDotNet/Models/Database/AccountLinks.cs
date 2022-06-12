29654d25906c634d0f158d565c9a000de8f0177d
Eris <erisapps@outlook.be>
2022-06-13 01:03:04 +0200

    Introduce new UserSettings database models

---
+++ POI.DiscordDotNet/Models/Database/AccountLinks.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace POI.DiscordDotNet.Models.Database
+{
+	public class AccountLinks
+	{
+		public string? ScoreSaberId { get; set; }
+
+		[BsonConstructor]
+		public AccountLinks(string? scoreSaberId)
+		{
+			ScoreSaberId = scoreSaberId;
+		}
+	}
+}
---
+++ POI.DiscordDotNet/Models/Database/UserSettings.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using NodaTime;
+
+namespace POI.DiscordDotNet.Models.Database
+{
+	public class UserSettings
+	{
+		[BsonId]
+		public string DiscordId { get; set; }
+
+		public LocalDate? Birthday { get; set; }
+
+		public AccountLinks AccountLinks { get; set; }
+
+		[BsonConstructor]
+		public UserSettings(string discordId, LocalDate? birthday, AccountLinks accountLinks)
+		{
+			DiscordId = discordId;
+			Birthday = birthday;
+			AccountLinks = accountLinks;
+		}
+	}
+}
