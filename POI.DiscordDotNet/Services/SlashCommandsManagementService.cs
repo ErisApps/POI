439e5fe5555e429d62287b39b4dc2f5155e78056
Eris <erisapps@outlook.be>
2022-06-12 21:06:38 +0200

    Move slashcommand init logic to its own service

---
+++ POI.DiscordDotNet/Services/SlashCommandsManagementService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.SlashCommands.EventArgs;
+using Microsoft.Extensions.Logging;
+using POI.DiscordDotNet.Commands.Utils;
+
+namespace POI.DiscordDotNet.Services
+{
+	public class SlashCommandsManagementService : IDisposable
+	{
+		private readonly IServiceProvider _serviceProvider;
+		private readonly ILogger<SlashCommandsManagementService> _logger;
+		private readonly DiscordClient _discordClient;
+
+		private SlashCommandsExtension? _slashCommands;
+
+		public SlashCommandsManagementService(IServiceProvider serviceProvider, ILogger<SlashCommandsManagementService> logger, DiscordClient discordClient)
+		{
+			_serviceProvider = serviceProvider;
+			_logger = logger;
+			_discordClient = discordClient;
+		}
+
+		public void Setup()
+		{
+			_slashCommands = _discordClient.UseSlashCommands(new SlashCommandsConfiguration { Services = _serviceProvider });
+			_slashCommands.SlashCommandErrored += OnSlashCommandErrored;
+			_slashCommands.SlashCommandExecuted += OnSlashCommandsExecuted;
+
+			// TODO: Register slash commands below
+			_slashCommands.RegisterCommands<PingCommand>();
+			_slashCommands.RegisterCommands<UptimeCommand>();
+		}
+
+		public void Dispose()
+		{
+			if (_slashCommands == null)
+			{
+				return;
+			}
+
+			_slashCommands.SlashCommandErrored -= OnSlashCommandErrored;
+			_slashCommands.SlashCommandExecuted -= OnSlashCommandsExecuted;
+			_slashCommands = null;
+		}
+
+		private Task OnSlashCommandErrored(SlashCommandsExtension _, SlashCommandErrorEventArgs eventArgs)
+		{
+			_logger.LogError(eventArgs.Exception, "{Username} tried to execute slashcommand /{CommandName}, but it errored", eventArgs.Context.User.Username, eventArgs.Context.CommandName);
+
+			return Task.CompletedTask;
+		}
+
+		private Task OnSlashCommandsExecuted(SlashCommandsExtension _, SlashCommandExecutedEventArgs eventArgs)
+		{
+			_logger.LogDebug("{Username} executed slashcommand /{CommandName}", eventArgs.Context.User.Username, eventArgs.Context.CommandName);
+
+			return Task.CompletedTask;
+		}
+	}
+}
