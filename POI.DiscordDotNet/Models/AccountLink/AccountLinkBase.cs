fffc9f0d11d4fd3700aa8e3bc2ffcc245477118f
Eris <erisapps@outlook.be>
2022-06-13 17:53:17 +0200

    Future-proof logic for querying all accountlinks of a certain type

---
+++ POI.DiscordDotNet/Models/AccountLink/AccountLinkBase.cs
@@ -0,0 +1,12 @@
+﻿namespace POI.DiscordDotNet.Models.AccountLink
+{
+	public abstract class AccountLinkBase
+	{
+		public string DiscordId { get; }
+
+		protected AccountLinkBase(string discordId)
+		{
+			DiscordId = discordId;
+		}
+	}
+}
