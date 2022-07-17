15cc751b307c7f695cb8f7cbfac0d2744c4f44ac
Eris <erisapps@outlook.be>
2022-07-17 15:55:36 +0200

    Fixed bug when incorrect date format was being used

--- POI.DiscordDotNet/Commands/Profile/BirthdaySlashCommandsModule.cs
+++ POI.DiscordDotNet/Commands/Profile/BirthdaySlashCommandsModule.cs
@@ -12,21 +12,28 @@ namespace POI.DiscordDotNet.Commands.Profile
 		{
 			private readonly UserSettingsService _userSettingsService;
 
+			private readonly LocalDatePattern _localDatePattern;
+
 			public BirthdaySlashCommandsModule(UserSettingsService userSettingsService)
 			{
 				_userSettingsService = userSettingsService;
+
+				_localDatePattern = LocalDatePattern.CreateWithInvariantCulture("dd'-'MM'-'uuuu");
 			}
 
 			[SlashCommand("set", "Sets your birthday")]
-			public async Task Set(InteractionContext ctx, [Option("date", "The date of your birthday. (Format: yyyy-MM-dd)")] string birthdayDateRaw)
+			public async Task Set(InteractionContext ctx, [Option("date", "The date of your birthday. Format: dd-MM-yyyy Example: 31-10-1998")] string birthdayDateRaw)
 			{
-				var parseResult = LocalDatePattern.Iso.Parse(birthdayDateRaw);
+				var parseResult = _localDatePattern.Parse(birthdayDateRaw);
 				if (parseResult.Success)
 				{
 					await _userSettingsService.UpdateBirthday(ctx.User.Id.ToString(), parseResult.Value).ConfigureAwait(false);
+					await ctx.CreateResponseAsync("Birthday has been updated").ConfigureAwait(false);
 				}
-
-				await ctx.CreateResponseAsync("Birthday has been updated").ConfigureAwait(false);
+				else
+				{
+					await ctx.CreateResponseAsync("Couldn't parse the birthday date, please make sure that you used the correct format. Format: dd-MM-yyyy Example: 31-10-1998").ConfigureAwait(false);
+				}
 			}
 
 			[SlashCommand("clear", "Unsets your birthday")]
