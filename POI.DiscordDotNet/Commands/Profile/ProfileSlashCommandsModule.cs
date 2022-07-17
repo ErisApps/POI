a8db17874c52ba0fb922e6ec8a458444e729acb6
Eris <erisapps@outlook.be>
2022-07-17 14:27:44 +0200

    Added commands to set/unset birthday

---
+++ POI.DiscordDotNet/Commands/Profile/ProfileSlashCommandsModule.cs
@@ -0,0 +1,9 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace POI.DiscordDotNet.Commands.Profile
+{
+	[SlashCommandGroup("profile", "Personal profile related commands")]
+	public partial class ProfileSlashCommandsModule : ApplicationCommandModule
+	{
+	}
+}
