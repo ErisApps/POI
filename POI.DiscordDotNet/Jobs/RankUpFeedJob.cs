14772545400fc1de9af318715a7d4c36d615db30
Eris <erisapps@outlook.be>
2022-09-18 18:45:32 +0200

    Refactor RankUpFeed logic

--- POI.DiscordDotNet/Jobs/RankUpFeedJob.cs
+++ POI.DiscordDotNet/Jobs/RankUpFeedJob.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.Entities;
@@ -11,6 +10,7 @@ using MongoDB.Driver;
 using POI.Core.Models.ScoreSaber.Profile;
 using POI.Core.Services;
 using POI.DiscordDotNet.Extensions;
+using POI.DiscordDotNet.Models.AccountLink;
 using POI.DiscordDotNet.Models.Database;
 using POI.DiscordDotNet.Services;
 using Quartz;
@@ -28,7 +28,8 @@ namespace POI.DiscordDotNet.Jobs
 		private readonly UserSettingsService _userSettingsService;
 		private readonly MongoDbService _mongoDbService;
 
-		private readonly SemaphoreSlim _concurrentExecutionSemaphoreSlim = new(1, 1);
+		private readonly string[] _countryDefinition = { "BE" };
+		private readonly string[] _profileRefreshExclusions = Array.Empty<string>();
 
 		public RankUpFeedJob(ILogger<RankUpFeedJob> logger, DiscordClient discordClient, ScoreSaberApiService scoreSaberApiService, UserSettingsService userSettingsService,
 			MongoDbService mongoDbService)
@@ -42,25 +43,6 @@ namespace POI.DiscordDotNet.Jobs
 
 		public async Task Execute(IJobExecutionContext context)
 		{
-			var acquiredLock = await _concurrentExecutionSemaphoreSlim.WaitAsync(0).ConfigureAwait(false);
-			if (!acquiredLock)
-			{
-				_logger.LogWarning("Couldn't acquire lock. Job is still running...");
-				return;
-			}
-
-			try
-			{
-				await ExecuteInternal(context).ConfigureAwait(false);
-			}
-			finally
-			{
-				_concurrentExecutionSemaphoreSlim.Release();
-			}
-		}
-
-		private async Task ExecuteInternal(IJobExecutionContext context)
-		{
 			_logger.LogInformation("Executing RankUpFeed logic");
 
 			var allScoreSaberLinks = await _userSettingsService.GetAllScoreSaberAccountLinks().ConfigureAwait(false);
@@ -72,86 +54,86 @@ namespace POI.DiscordDotNet.Jobs
 				return;
 			}
 
-			var countryDefinition = new[] { "BE" };
-
 			var playersWrappers = await Task.WhenAll(Enumerable
-				.Range(1, TOP/50)
-				.Select(page => _scoreSaberApiService.FetchPlayers((uint) page, countries: countryDefinition)));
-
+				.Range(1, TOP / 50)
+				.Select(page => _scoreSaberApiService.FetchPlayers((uint) page, countries: _countryDefinition)));
 			if (playersWrappers.Any(x => x == null))
 			{
 				return;
 			}
 
 			var roles = OrderTopRoles(guild.Roles.Where(x => x.Value.Name.Contains("(Top ", StringComparison.Ordinal)));
-
 			var players = playersWrappers.SelectMany(x => x!.Players).ToList();
 			foreach (var player in players)
 			{
-				// _logger.LogDebug("#{Rank} {Name}", player.CountryRank, player.Name);
-				if (player.ProfilePicture.EndsWith("steam.png") && player.Id.Length == 17 && player.Id.StartsWith("7"))
+				await HandlePlayer(player, allScoreSaberLinks, members, roles);
+			}
+
+
+			var leaderboardEntriesCollection = _mongoDbService.GetCollection<LeaderboardEntry>();
+			var originalLeaderboardEntries = await (await leaderboardEntriesCollection.FindAsync(_ => true).ConfigureAwait(false)).ToListAsync().ConfigureAwait(false);
+			await PostChangesOnDiscord(guild, originalLeaderboardEntries, players);
+
+			_ = await leaderboardEntriesCollection.DeleteManyAsync(_ => true).ConfigureAwait(false);
+			await leaderboardEntriesCollection.InsertManyAsync(players.Select(p => new LeaderboardEntry(p.Id, p.Name, p.CountryRank, p.Pp))).ConfigureAwait(false);
+		}
+
+		private async Task HandlePlayer(ProfileBase player,
+			IEnumerable<ScoreSaberAccountLink> scoreSaberLinks,
+			IEnumerable<DiscordMember> members,
+			IReadOnlyCollection<(uint? RankThreshold, DiscordRole Role)> roles)
+		{
+			// _logger.LogDebug("#{Rank} {Name}", player.CountryRank, player.Name);
+			await TriggerProfileRefreshIfNeeded(player);
+
+			var discordId = scoreSaberLinks.FirstOrDefault(x => x.ScoreSaberId == player.Id)?.DiscordId;
+			if (discordId == null)
+			{
+				// _logger.LogWarning("No ScoreLink found for player {PlayerName}", player.Name);
+				return;
+			}
+
+			var member = members.FirstOrDefault(x => string.Equals(x.Id.ToString(), discordId, StringComparison.Ordinal));
+			if (member == null)
+			{
+				// _logger.LogWarning("ScoreLink exists for non-Discord-member. ScoreSaber name: {PlayerName}, ScoreSaberId: {PlayerId}", player.Name, player.Id);
+				return;
+			}
+
+			var currentTopRoles = member.Roles.Where(x => x.Name.Contains("(Top ", StringComparison.Ordinal)).ToList();
+			// _logger.LogDebug("Currently has role {RoleName}", string.Join(", ", currentTopRoles.Select(x => x.Name)));
+
+			var applicableRole = DetermineApplicableRole(roles, player.Rank);
+
+			// Check whether the applicable role is granted
+			if (currentTopRoles.All(role => role.Id != applicableRole.Id))
+			{
+				_logger.LogInformation("Granting {PlayerName} role {RoleName}", member.DisplayName, applicableRole.Name);
+				await member.GrantRoleAsync(applicableRole, "RankUpFeed update").ConfigureAwait(false);
+			}
+
+			// Revoke all other top roles if needed
+			foreach (var revocableRole in currentTopRoles.Where(role => role.Id != applicableRole.Id))
+			{
+				_logger.LogInformation("Revoking role {RoleName} for {PlayerName}", revocableRole.Name, member.DisplayName);
+				await member.RevokeRoleAsync(revocableRole, "RankUpFeed update").ConfigureAwait(false);
+			}
+		}
+
+		private async Task TriggerProfileRefreshIfNeeded(ProfileBase player)
+		{
+			if (player.ProfilePicture.EndsWith("steam.png") && player.Id.Length == 17 && player.Id.StartsWith("7"))
+			{
+				if (_profileRefreshExclusions.Contains(player.Id))
+				{
+					_logger.LogWarning("Refresh prevented for player {PlayerName} at rank {PlayerRank} due to explicit exclusion", player.Name, player.CountryRank);
+				}
+				else
 				{
 					_logger.LogInformation("Calling Refresh for player {PlayerName} at rank {PlayerRank}", player.Name, player.CountryRank);
 					await _scoreSaberApiService.RefreshProfile(player.Id).ConfigureAwait(false);
 				}
-
-				var discordId = allScoreSaberLinks.FirstOrDefault(x => x.ScoreSaberId == player.Id)?.DiscordId;
-				if (discordId == null)
-				{
-					// _logger.LogWarning("Has no link");
-					continue;
-				}
-
-				var member = members.FirstOrDefault(x => string.Equals(x.Id.ToString(), discordId, StringComparison.Ordinal));
-				if (member == null)
-				{
-					// _logger.LogWarning("ScoreLink exists for non-member");
-					continue;
-				}
-
-				var currentTopRoles = member.Roles.Where(x => x.Name.Contains("(Top ", StringComparison.Ordinal)).ToList();
-				// _logger.LogDebug("Currently has role {RoleName}", string.Join(", ", currentTopRoles.Select(x => x.Name)));
-
-				var applicableRole = DetermineApplicableRole(roles, player.Rank);
-
-				// Check whether the applicable role is granted
-				if (currentTopRoles.All(role => role.Id != applicableRole.Id))
-				{
-					_logger.LogInformation("Granting {PlayerName} role {RoleName}", member.DisplayName, applicableRole.Name);
-					await member.GrantRoleAsync(applicableRole, "RankUpFeed update").ConfigureAwait(false);
-				}
-
-				// Revoke all other top roles if needed
-				foreach (var revocableRole in currentTopRoles.Where(role => role.Id != applicableRole.Id))
-				{
-					_logger.LogInformation("Revoking role {RoleName} for {PlayerName}", revocableRole.Name, member.DisplayName);
-					await member.RevokeRoleAsync(revocableRole, "RankUpFeed update").ConfigureAwait(false);
-				}
 			}
-
-			var leaderboardEntriesCollection = _mongoDbService.GetCollection<LeaderboardEntry>();
-			var originalLeaderboardEntries = await (await leaderboardEntriesCollection.FindAsync(_ => true).ConfigureAwait(false)).ToListAsync().ConfigureAwait(false);
-			var rankedUpPlayers = DetermineRankedUpPlayers(originalLeaderboardEntries, players);
-
-			var rankUpFeedChannel = guild.GetChannel(634091663526199307);
-			if (rankUpFeedChannel != null)
-			{
-				foreach (var player in rankedUpPlayers)
-				{
-					var rankUpEmbed = new DiscordEmbedBuilder()
-						.WithPoiColor()
-						.WithTitle($"Well done, {player.Name}")
-						.WithUrl($"https://scoresaber.com/u/{player.Id}")
-						.WithThumbnail(player.ProfilePicture)
-						.WithDescription($"{player.Name} is now rank **#{player.CountryRank}** of the BE beat saber players with a total pp of **{player.Pp}**")
-						.Build();
-
-					await rankUpFeedChannel.SendMessageAsync(rankUpEmbed).ConfigureAwait(false);
-				}
-			}
-
-			_ = await leaderboardEntriesCollection.DeleteManyAsync(_ => true).ConfigureAwait(false);
-			await leaderboardEntriesCollection.InsertManyAsync(players.Select(p => new LeaderboardEntry(p.Id, p.Name, p.CountryRank, p.Pp))).ConfigureAwait(false);
 		}
 
 		private static List<(uint? RankThreshold, DiscordRole Role)> OrderTopRoles(IEnumerable<KeyValuePair<ulong, DiscordRole>> unorderedTopRoles)
@@ -185,6 +167,28 @@ namespace POI.DiscordDotNet.Jobs
 			}
 
 			return applicableRole;
+		}
+
+		private static async Task PostChangesOnDiscord(DiscordGuild guild, IReadOnlyCollection<LeaderboardEntry> originalLeaderboardEntries, List<ExtendedBasicProfile> players)
+		{
+			var rankedUpPlayers = DetermineRankedUpPlayers(originalLeaderboardEntries, players);
+
+			var rankUpFeedChannel = guild.GetChannel(634091663526199307);
+			if (rankUpFeedChannel != null)
+			{
+				foreach (var player in rankedUpPlayers)
+				{
+					var rankUpEmbed = new DiscordEmbedBuilder()
+						.WithPoiColor()
+						.WithTitle($"Well done, {player.Name}")
+						.WithUrl($"https://scoresaber.com/u/{player.Id}")
+						.WithThumbnail(player.ProfilePicture)
+						.WithDescription($"{player.Name} is now rank **#{player.CountryRank}** of the BE beat saber players with a total pp of **{player.Pp}**")
+						.Build();
+
+					await rankUpFeedChannel.SendMessageAsync(rankUpEmbed).ConfigureAwait(false);
+				}
+			}
 		}
 
 		private static List<ExtendedBasicProfile> DetermineRankedUpPlayers(IReadOnlyCollection<LeaderboardEntry> originalLeaderboard, List<ExtendedBasicProfile> currentLeaderboard)
