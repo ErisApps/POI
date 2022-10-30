d69458ae37bbfde5fa4374a9b19e62e3d1f3feed
Eris <erisapps@outlook.be>
2022-10-30 20:53:07 +0100

    Update docs of ServiceCollectionExtensions in ThirdParty projects

--- Src/POI.ThirdParty.ScoreSaber/Extensions/ServiceCollectionExtensions.cs
+++ Src/POI.ThirdParty.ScoreSaber/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using POI.Core.Services;
 using POI.ThirdParty.ScoreSaber.Services;
 using POI.ThirdParty.ScoreSaber.Services.Implementations;
 
@@ -9,11 +8,11 @@ namespace POI.ThirdParty.ScoreSaber.Extensions;
 public static class ServiceCollectionExtensions
 {
 	///  <summary>
-	///		Registers the services of the Core project
+	///		Registers the services of the POI.ThirdParty.ScoreSaber project
 	///  </summary>
 	///  <param name="serviceCollection">The container in which the services of the project have to be registered</param>
 	///  <remark>
-	///		Make sure that the <see cref="IConstantsCore">IConstants</see> and <see cref="Microsoft.Extensions.Logging.ILogger">ILogger</see> interfaces has been
+	///		Make sure that the <see cref="POI.Core.Services.IConstantsCore">IConstants</see> and <see cref="Microsoft.Extensions.Logging.ILogger">ILogger</see> interfaces have been
 	///		registered before calling this method
 	///  </remark>
 	public static IServiceCollection AddScoreSaber(this IServiceCollection serviceCollection)
