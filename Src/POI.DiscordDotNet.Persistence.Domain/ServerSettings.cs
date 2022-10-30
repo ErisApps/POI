730c4e3c299169319550aac5240bdf85414b8fd0
Eris <erisapps@outlook.be>
2022-10-30 23:06:11 +0100

    Add POI.DiscordDotNet.Persistence.Domain project

---
+++ Src/POI.DiscordDotNet.Persistence.Domain/ServerSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace POI.DiscordDotNet.Persistence.Domain
+{
+	public class ServerSettings
+	{
+		[Key]
+		public ulong ServerId { get; init; }
+
+		public ulong? RankUpFeedChannelId { get; set; }
+
+		public ulong? BirthdayRoleId { get; set; }
+
+		public static ServerSettings CreateDefault(ulong serverId)
+		{
+			return new ServerSettings
+			{
+				ServerId = serverId
+			};
+		}
+	}
+}
