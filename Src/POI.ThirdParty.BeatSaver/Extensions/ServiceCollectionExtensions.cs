c919bf1928943be78bab57e058347882db9f3165
Eris <erisapps@outlook.be>
2022-11-07 00:22:16 +0100

    Fix incorrect remark for BeatSaver project registration method

--- Src/POI.ThirdParty.BeatSaver/Extensions/ServiceCollectionExtensions.cs
+++ Src/POI.ThirdParty.BeatSaver/Extensions/ServiceCollectionExtensions.cs
@@ -12,8 +12,7 @@ public static class ServiceCollectionExtensions
 	///  </summary>
 	///  <param name="serviceCollection">The container in which the services of the project have to be registered</param>
 	///  <remark>
-	///		Make sure that the <see cref="POI.Core.Services.IConstantsCore">IConstants</see> and <see cref="Microsoft.Extensions.Logging.ILogger">ILogger</see> interfaces have been
-	///		registered before calling this method
+	///		Make sure that the <see cref="POI.Core.Services.IConstantsCore">IConstants</see> interface has been registered before calling this method
 	///  </remark>
 	public static IServiceCollection AddBeatSaver(this IServiceCollection serviceCollection)
 	{
