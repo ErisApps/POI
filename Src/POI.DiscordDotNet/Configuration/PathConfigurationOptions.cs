241c9b79b88bbe906ef05b5155946a517167f86e
Eris <erisapps@outlook.be>
2022-12-18 23:14:02 +0100

    Fix some nullability warnings

--- Src/POI.DiscordDotNet/Configuration/PathConfigurationOptions.cs
+++ Src/POI.DiscordDotNet/Configuration/PathConfigurationOptions.cs
@@ -4,7 +4,7 @@ public class PathConfigurationOptions
 {
 	public const string SECTION_NAME = "Paths";
 
-	public string DataFolderPath { get; init; }
+	public required string DataFolderPath { get; init; }
 
 	public string? AssetsFolderPath { get; init; }
 	public string? LogsFolderPath { get; init; }
