ebe221e81bb2cb23e51ea5558e718e061fa96aca
GianniKoch <johannes.koch@student.kdg.be>
2023-06-19 11:13:08 +0200

    Applied requested changes for parent branch and current branch.

--- Src/POI.DiscordDotNet/Services/Implementations/DiscordStarboardService.cs
+++ Src/POI.DiscordDotNet/Services/Implementations/DiscordStarboardService.cs
@@ -9,12 +9,12 @@ namespace POI.DiscordDotNet.Services.Implementations;
 
 public class DiscordStarboardService : IAddDiscordClientFunctionality
 {
-	private readonly ILogger<DiscordChatCommandsService> _logger;
+	private readonly ILogger<DiscordStarboardService> _logger;
 	private readonly IStarboardMessagesRepository _starboardMessagesRepository;
 	private readonly IServerSettingsRepository _serverSettingsRepository;
 
 	public DiscordStarboardService(
-		ILogger<DiscordChatCommandsService> logger,
+		ILogger<DiscordStarboardService> logger,
 		IStarboardMessagesRepository starboardMessagesRepository, IServerSettingsRepository serverSettingsRepository)
 	{
 		_logger = logger;
@@ -40,30 +40,33 @@ public class DiscordStarboardService : IAddDiscordClientFunctionality
 
 	private async Task OnReactionAdded(DiscordClient sender, MessageReactionAddEventArgs args)
 	{
-		//TODO: Check if message was not cached
 		//TODO: Check on emoji ID, not name
 		//TODO: Figure out a way to get the display name of the user outside the guild...
 
-		var serverSettings = await _serverSettingsRepository.FindOneById(args.Channel.Guild.Id);
+		var guild = args.Guild;
+		var message = args.Message;
+		var channel = args.Channel;
+
+		var serverSettings = await _serverSettingsRepository.FindOneById(guild.Id);
 		if (serverSettings?.StarboardChannelId == null)
 		{
-			_logger.LogError("Server settings or starboard channel id not found!");
+			_logger.LogWarning("Server settings or starboard channel id not found for guild id {GuildId}!", guild.Id);
 			return;
 		}
 
 		// Skip event if the message is in the starboard channel (To prevent people staring the bot messages)
-		if(args.Channel.Id == serverSettings.StarboardChannelId)
+		if (channel.Id == serverSettings.StarboardChannelId)
 		{
 			return;
 		}
 
 		// Check if the message reactions contains the star emote
-		if (args.Message.Reactions.All(x => x.Emoji.Name != "⭐"))
+		if (message.Reactions.All(x => x.Emoji.Name != "⭐"))
 		{
 			return;
 		}
 
-		var messageStarCount = args.Message.Reactions.First(x => x.Emoji.Name == "⭐").Count;
+		var messageStarCount = message.Reactions.First(x => x.Emoji.Name == "⭐").Count;
 		// Check if the message has enough stars
 		if (messageStarCount < serverSettings.StarboardEmojiCount)
 		{
@@ -79,31 +82,33 @@ public class DiscordStarboardService : IAddDiscordClientFunctionality
 		}
 
 		// Check if the message is cached and get contents if true.
-		var message = args.Message;
-		if (args.Message.Author == null)
+		if (message.Author == null)
 		{
-			message = await args.Channel.GetMessageAsync(args.Message.Id, true);
+			message = await channel.GetMessageAsync(message.Id, true);
 		}
 
-		var embed = GetStarboardEmbed(message.Author.Username, args.Message.Channel.Name, message.Content, message.JumpLink, message.Timestamp, (uint)messageStarCount, message.Attachments.FirstOrDefault()?.Url);
+		var embed = GetStarboardEmbed(message.Author.Username, message.Channel.Name, message.Content, message.JumpLink, message.Timestamp, (uint) messageStarCount,
+			message.Attachments.FirstOrDefault()?.Url);
 
 		// Get the starboard message from the database
-		var foundMessage = await _starboardMessagesRepository.FindOneByServerIdAndChannelIdAndMessageId(args.Guild.Id, args.Channel.Id, args.Message.Id);
-		// If the message is already in the database, update the star count
-		// (This will also update the message contents)
-		if(foundMessage != null)
-		{
-			var starboardMessage = await starboardChannel.GetMessageAsync(foundMessage.StarboardMessageId);
-			await starboardMessage.ModifyAsync(msg => msg.Embed = embed);
-			_logger.LogInformation("Updated message {JumpLink} with {stars} stars!", args.Message.JumpLink, messageStarCount);
-			return;
-		}
+		var foundMessage = await _starboardMessagesRepository.FindOneByServerIdAndChannelIdAndMessageId(guild.Id, channel.Id, message.Id);
 
 		// If the message is not in the database, create a new starboard message
-		var embedMessage = await starboardChannel.SendMessageAsync(embed);
-		// And add to the database.
-		await _starboardMessagesRepository.Insert(new StarboardMessages(args.Guild.Id, args.Channel.Id, args.Message.Id, embedMessage.Id));
-		_logger.LogInformation("Message {JumpLink} sent to starboard channel!", args.Message.JumpLink);
+		if (foundMessage == null)
+		{
+			var embedMessage = await starboardChannel.SendMessageAsync(embed);
+			// And add to the database.
+			await _starboardMessagesRepository.Insert(new StarboardMessages(guild.Id, channel.Id, message.Id, embedMessage.Id));
+			_logger.LogInformation("Message {JumpLink} sent to starboard channel!", message.JumpLink);
+		}
+		else
+		{
+			// Else the message is already in the database, update the star count
+			// (This will also update the message contents)
+			var starboardMessage = await starboardChannel.GetMessageAsync(foundMessage.StarboardMessageId);
+			await starboardMessage.ModifyAsync(msg => msg.Embed = embed);
+			_logger.LogInformation("Updated message {JumpLink} with {Stars} stars!", message.JumpLink, messageStarCount);
+		}
 	}
 
 	private static DiscordEmbed GetStarboardEmbed(string userName, string channelName, string content, Uri jumpLink, DateTimeOffset timestamp, uint messageStarCount, string? attachmentUrl = null)
@@ -111,11 +116,10 @@ public class DiscordStarboardService : IAddDiscordClientFunctionality
 		var builder = new DiscordEmbedBuilder()
 			.WithTitle($"{userName} in #{channelName}")
 			.WithDescription(content)
-			// .WithDescription(content.Length > 50 ? content[..50] + "..." : content)
 			.WithColor(new DiscordColor(0x6CCAF1))
 			.WithUrl(jumpLink)
 			.WithFooter($"⭐{messageStarCount}")
-			.WithTimestamp(TimeZoneInfo.ConvertTimeBySystemTimeZoneId(timestamp, "Europe/Brussels"));
+			.WithTimestamp(timestamp);
 
 		if (!string.IsNullOrWhiteSpace(attachmentUrl))
 		{
