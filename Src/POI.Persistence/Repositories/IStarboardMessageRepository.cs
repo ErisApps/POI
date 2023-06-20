219c00fdf88c6a7f2666005c2a7ffe517ee0dad0
GianniKoch <johannes.koch@student.kdg.be>
2023-06-20 20:46:05 +0200

    Fix typo repository name.

--- Src/POI.Persistence/Repositories/IStarboardMessageRepository.cs
+++ Src/POI.Persistence/Repositories/IStarboardMessageRepository.cs
@@ -2,7 +2,7 @@
 
 namespace POI.Persistence.Repositories;
 
-public interface IStarboardMessagesRepository
+public interface IStarboardMessageRepository
 {
 	Task<StarboardMessages?> FindOneByServerIdAndChannelIdAndMessageId(ulong serverId, ulong channelId, ulong messageId, CancellationToken cts = default);
 	Task Insert(StarboardMessages entry, CancellationToken cts = default);
